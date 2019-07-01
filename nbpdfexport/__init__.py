fd82ab13daa7bd090a774ceeef406229c9685a37
yuvipanda <yuvipanda@gmail.com>
2019-07-01 15:25:26 -0700

    Add initial convertor content

---
+++ nbpdfexport/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Generate PDF with cells with just given tag.
+
+I can never figure out nbconvert's API, unfortunately.
+"""
+import argparse
+import nbconvert
+import tempfile
+import asyncio
+import tempfile
+from pyppeteer import launch
+
+
+async def html_to_pdf(html_file, pdf_file):
+    """
+    Convert arbitrary HTML file to PDF
+    """
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(f'file:///{html_file}')
+    await page.pdf({'path': pdf_file})
+    await browser.close()
+
+
+def notebook_to_pdf(notebook_path, pdf_path):
+    """
+    Convert given notebook file to PDF
+    """
+    exporter = nbconvert.HTMLExporter(config={})
+    exporter.exclude_input_prompt = True
+    exported_html, _ = exporter.from_filename(notebook_path)
+
+    with tempfile.NamedTemporaryFile(suffix='.html') as f:
+        f.write(exported_html.encode())
+        f.flush()
+        asyncio.get_event_loop().run_until_complete(html_to_pdf(f.name, pdf_path))
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        'notebook_path',
+        help='Path to .ipynb file to convert'
+    )
+    argparser.add_argument(
+        'pdf_path',
+        help='Path to output PDF file to'
+    )
+
+    args = argparser.parse_args()
+
+    notebook_to_pdf(args.notebook_path, args.pdf_path)
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nbpdfexport',
+    version='0.0.1',
+    install_requires=[
+        'nbconvert',
+        'pyppeteer',
+    ],
+    python_requires='>=3.6',
+    description='Convert Jupyter Notebooks to PDF without LaTeX',
+    url='http://github.com/yuvipanda/nbpdfexport',
+    author='Yuvi Panda',
+    author_email='yuvipanda@gmail.com',
+    license='BSD',
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': ['jupyter-pdfexport=nbpdfexport:main']
+    }
+)
