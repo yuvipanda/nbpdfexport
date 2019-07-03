c341be7ba2c862541a7276ef5ab43e59e509b1e7
yuvipanda <yuvipanda@gmail.com>
2019-07-02 18:08:21 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nbpdfexport',
-    version='0.1',
+    version='0.2',
     install_requires=[
         'nbconvert',
         'pyppeteer',
